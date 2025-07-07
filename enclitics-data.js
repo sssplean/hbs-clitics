b1c5a39ec4c1cb79790a7bc19ac15638ef27f0cd
Vladimir Volkov <106834173+sssplean@users.noreply.github.com>
2025-07-07 23:19:30 +0200

    Update enclitics-data.js

--- enclitics-data.js
+++ enclitics-data.js
@@ -1,97 +1,188 @@
-const encliticsExamples = [
+export const encliticsExamples = [
   {
-    "parts": [
-      "Ana",
-      "vidjela",
-      "Marka",
-      "jučer."
-    ],
-    "enclitics": [
-      "je"
-    ],
-    "correctIndexes": [
-      1
-    ],
-    "distractors": [
-      "mu",
-      "li"
-    ]
+    "parts": ["Petar", "dao", "poklon", "djetetu", "jučer"],
+    "enclitics": ["mu", "je"],
+    "distractors": ["ga", "se"],
+    "correctIndexes": [1, 2]
   },
   {
-    "parts": [
-      "On",
-      "došao",
-      "rano",
-      "jutros."
-    ],
-    "enclitics": [
-      "je"
-    ],
-    "correctIndexes": [
-      1
-    ],
-    "distractors": [
-      "mu",
-      "ga"
-    ]
+    "parts": ["Ona", "rekla", "da", "će", "doći"],
+    "enclitics": ["mi"],
+    "distractors": ["ti", "mu"],
+    "correctIndexes": [0]
   },
   {
-    "parts": [
-      "Nikada",
-      "nisam",
-      "video."
-    ],
-    "enclitics": [
-      "ga"
-    ],
-    "correctIndexes": [
-      2
-    ],
-    "distractors": [
-      "mu",
-      "li"
-    ]
+    "parts": ["Nisam", "znao", "da", "to", "možemo", "učiniti"],
+    "enclitics": ["ga"],
+    "distractors": ["se", "mu"],
+    "correctIndexes": [3]
   },
   {
-    "parts": [
-      "Petar",
-      "dao",
-      "poklon",
-      "djetetu",
-      "jučer."
-    ],
-    "enclitics": [
-      "mu",
-      "je"
-    ],
-    "correctIndexes": [
-      1,
-      2
-    ],
-    "distractors": [
-      "ga",
-      "se"
-    ]
+    "parts": ["On", "je", "htio", "da", "mi", "pomogne"],
+    "enclitics": ["se"],
+    "distractors": ["ga", "je"],
+    "correctIndexes": [1]
   },
   {
-    "parts": [
-      "Rekao",
-      "da",
-      "vidjeli",
-      "Marka",
-      "jučer."
-    ],
-    "enclitics": [
-      "su",
-      "ga"
-    ],
-    "correctIndexes": [
-      1,
-      2
-    ],
-    "distractors": [
-      "li",
-      "je"
-    ]
+    "parts": ["Rekli", "su", "nam", "da", "će", "doći"],
+    "enclitics": ["je"],
+    "distractors": ["mu", "ti"],
+    "correctIndexes": [0]
+  },
+  {
+    "parts": ["Vidjeli", "smo", "kako", "on", "radi"],
+    "enclitics": ["ga"],
+    "distractors": ["je", "mi"],
+    "correctIndexes": [2]
+  },
+  {
+    "parts": ["Nadam", "se", "da", "će", "on", "doći"],
+    "enclitics": ["mu"],
+    "distractors": ["ti", "je"],
+    "correctIndexes": [2]
+  },
+  {
+    "parts": ["Moramo", "se", "odlučiti", "šta", "da", "radimo"],
+    "enclitics": ["ga"],
+    "distractors": ["mi", "je"],
+    "correctIndexes": [3]
+  },
+  {
+    "parts": ["Nisam", "to", "rekao", "njemu"],
+    "enclitics": ["mu"],
+    "distractors": ["je", "se"],
+    "correctIndexes": [2]
+  },
+  {
+    "parts": ["Ana", "je", "poslala", "pismo", "majci"],
+    "enclitics": ["ga"],
+    "distractors": ["se", "mu"],
+    "correctIndexes": [2]
+  },
+  {
+    "parts": ["Ne", "znam", "da", "li", "će", "doći"],
+    "enclitics": ["mi"],
+    "distractors": ["ti", "je"],
+    "correctIndexes": [3]
+  },
+  {
+    "parts": ["Pitaš", "se", "šta", "da", "radiš"],
+    "enclitics": ["ga"],
+    "distractors": ["se", "je"],
+    "correctIndexes": [3]
+  },
+  {
+    "parts": ["Čuli", "smo", "da", "je", "pao"],
+    "enclitics": ["se"],
+    "distractors": ["mu", "ti"],
+    "correctIndexes": [2]
+  },
+  {
+    "parts": ["Nije", "htio", "da", "ga", "vidi"],
+    "enclitics": ["je"],
+    "distractors": ["se", "mi"],
+    "correctIndexes": [0]
+  },
+  {
+    "parts": ["Volim", "da", "mu", "pričam", "priče"],
+    "enclitics": ["je"],
+    "distractors": ["ti", "se"],
+    "correctIndexes": [0]
+  },
+  {
+    "parts": ["Morali", "smo", "da", "ga", "čekamo"],
+    "enclitics": ["je"],
+    "distractors": ["mu", "ti"],
+    "correctIndexes": [1]
+  },
+  {
+    "parts": ["Rekla", "je", "da", "će", "doći"],
+    "enclitics": ["mi"],
+    "distractors": ["mu", "je"],
+    "correctIndexes": [0]
+  },
+  {
+    "parts": ["Nisam", "joj", "mogao", "reći", "istinu"],
+    "enclitics": ["je"],
+    "distractors": ["se", "mu"],
+    "correctIndexes": [2]
+  },
+  {
+    "parts": ["Govorio", "sam", "mu", "nekoliko", "puta"],
+    "enclitics": ["je"],
+    "distractors": ["se", "ti"],
+    "correctIndexes": [1]
+  },
+  {
+    "parts": ["Ne", "znam", "ko", "je", "to", "uradio"],
+    "enclitics": ["mu"],
+    "distractors": ["se", "ti"],
+    "correctIndexes": [2]
+  },
+  {
+    "parts": ["Vidio", "sam", "da", "ga", "nema"],
+    "enclitics": ["je"],
+    "distractors": ["mu", "ti"],
+    "correctIndexes": [1]
+  },
+  {
+    "parts": ["Ne", "volim", "kada", "mi", "to", "radiš"],
+    "enclitics": ["se"],
+    "distractors": ["ga", "je"],
+    "correctIndexes": [1]
+  },
+  {
+    "parts": ["Dali", "su", "mi", "knjigu"],
+    "enclitics": ["je"],
+    "distractors": ["ga", "ti"],
+    "correctIndexes": [0]
+  },
+  {
+    "parts": ["Objasnio", "sam", "joj", "pravila"],
+    "enclitics": ["se"],
+    "distractors": ["je", "ga"],
+    "correctIndexes": [1]
+  },
+  {
+    "parts": ["Poželjela", "sam", "da", "mu", "to", "kažem"],
+    "enclitics": ["je"],
+    "distractors": ["se", "ti"],
+    "correctIndexes": [1]
+  },
+  {
+    "parts": ["Kupio", "sam", "mu", "poklon"],
+    "enclitics": ["je"],
+    "distractors": ["ti", "se"],
+    "correctIndexes": [1]
+  },
+  {
+    "parts": ["Ne", "znam", "šta", "da", "mu", "kažem"],
+    "enclitics": ["je"],
+    "distractors": ["se", "ti"],
+    "correctIndexes": [1]
+  },
+  {
+    "parts": ["Zamolio", "sam", "je", "da", "pomogne"],
+    "enclitics": ["se"],
+    "distractors": ["mu", "ti"],
+    "correctIndexes": [2]
+  },
+  {
+    "parts": ["Ne", "mogu", "da", "mu", "to", "objasnim"],
+    "enclitics": ["je"],
+    "distractors": ["ga", "se"],
+    "correctIndexes": [1]
+  },
+  {
+    "parts": ["Mislim", "da", "je", "mi", "to", "rekao"],
+    "enclitics": ["se"],
+    "distractors": ["ga", "ti"],
+    "correctIndexes": [2]
+  },
+  {
+    "parts": ["Nikada", "mu", "nisam", "rekao", "istinu"],
+    "enclitics": ["je"],
+    "distractors": ["se", "ti"],
+    "correctIndexes": [2]
   }
 ];
