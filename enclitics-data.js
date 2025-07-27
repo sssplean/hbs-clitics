a572ef63330448ecb77f40411134b0c5eb37df68
Vladimir Volkov <106834173+sssplean@users.noreply.github.com>
2025-07-27 23:09:43 +0200

    Expand examples

--- enclitics-data.js
+++ enclitics-data.js
@@ -4,6 +4,34 @@ const encliticsExamples = [
     "enclitics": ["mu", "je", "za"],
     "distractors": ["ga", "se"],
     "correctIndexes": [2, 2.1, 5],
-    "translation": "Петар дал ему вчера подарок для ребёнка."
+    "translation": "Петар дал ему вчера подарок для ребёнка.",
+  },
+  {
+    "parts": ["Ana", "juče", "dala", "knjigu"],
+    "enclitics": ["mi", "je"],
+    "distractors": ["ga", "se", "li"],
+    "correctIndexes": [2, 2.1],
+    "translation": "Ана вчера дала мне книгу.",
+  },
+  {
+    "parts": ["Milan", "juče", "kupio", "auto"],
+    "enclitics": ["ga", "je"],
+    "distractors": ["mu", "se"],
+    "correctIndexes": [2, 2.1],
+    "translation": "Милан вчера купил его автомобиль.",
+  },
+  {
+    "parts": ["Sutra", "ću", "dati"],
+    "enclitics": ["ti", "ga"],
+    "distractors": ["mu", "je"],
+    "correctIndexes": [3, 3.1],
+    "translation": "Завтра я тебе его дам.",
+  },
+  {
+    "parts": ["Ona", "često", "pravi", "kolače"],
+    "enclitics": ["mu"],
+    "distractors": ["je", "ga", "se"],
+    "correctIndexes": [2],
+    "translation": "Она часто ему делает пирожные.",
   },
 ];
